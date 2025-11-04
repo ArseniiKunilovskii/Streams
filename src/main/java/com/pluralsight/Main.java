7c4068d413c811a30cfb2a8760939cd47eb41e9f
arsenii <arseniy.kunilovskiy@gmail.com>
2025-11-04 09:41:03 -0500

    Step 3 is done

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -9,6 +9,7 @@ public class Main {
         boolean done = false;
         Scanner scanner = new Scanner(System.in);
         String input;
+        int totalAge = 0;
 
         ArrayList<Person> people = new ArrayList<>();
         ArrayList<Person> foundPeople = new ArrayList<>();
@@ -18,11 +19,11 @@ public class Main {
         people.add(new Person("Charlie", "Brown", 55));
         people.add(new Person("Dana", "Miller", 41));
         people.add(new Person("Ethan", "Davis", 19));
-        people.add(new Person("Fiona", "Garcia", 62));
+        people.add(new Person("Fiona", "George", 62));
         people.add(new Person("George", "Rodriguez", 38));
         people.add(new Person("Hannah", "Martinez", 28));
         people.add(new Person("Ivy", "Hernandez", 70));
-        people.add(new Person("Jack", "Lopez", 33));
+        people.add(new Person("Jack", "George", 33));
 
         while(!done){
             System.out.println("Please enter name: (0 to exit)");
@@ -41,7 +42,9 @@ public class Main {
             System.out.println("People with name " + input);
             for (Person person: foundPeople){
                 System.out.println(person);
+                totalAge += person.getAge();
             }
+            System.out.println("Average age is " + (totalAge/foundPeople.size()));
         }
 
     }
